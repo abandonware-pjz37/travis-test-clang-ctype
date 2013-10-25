ef2d7fedb44bed9fd94be86dc98c06d6d5197fd9
ruslo <ruslan_baratov@yahoo.com>
2013-10-26 01:32:37 +0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,13 @@
-#include <cctype>
+#include <cctype> // std::isalnum
+#include <iostream> // std::cout
 
 int main() {
-  return std::isalnum('x') != 1;
+  if (std::isalnum('x') != 0) {
+    std::cout << "OK" << std::endl;
+    return EXIT_SUCCESS;
+  }
+  else {
+    std::cout << "FAILED" << std::endl;
+    return EXIT_FAILURE;
+  }
 }
